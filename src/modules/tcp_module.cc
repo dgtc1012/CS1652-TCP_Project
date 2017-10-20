69bb69fa288a2dd64aedf43f02fe3215eeb63c22
Dannah Gersh <drg42@pitt.edu>
2017-10-20 14:38:35 -0400

    added state machine

--- src/modules/tcp_module.cc
+++ src/modules/tcp_module.cc
@@ -137,3 +137,107 @@ int main(int argc, char * argv[]) {
 
     return 0;
 }
+
+void TCPStateMachine(TCPHeader &tcph, IPHeader &iph, ConnectionList<TCPState> &clist){
+    
+    Connection c;
+    unsigned char flags;
+    unsigned int ack;
+    unsigned int seqnum;
+    unsigned short window_size;
+    unsigned short urgent;
+    unsigned char tcph_size;
+    unsigned char iph_size;
+    unsigned short payload_size;
+    unsigned short checksum;
+
+    iph.GetDestIP(c.src);
+    iph.GetSourceIP(c.dest);
+    iph.GetProtocol(c.protocol);
+    //iph.GetHeaderLen(iph_size);
+
+    tcph.GetDestPort(c.srcport);
+    tcph.GetSourcePort(c.destport);
+    tcph.GetFlags(flags);
+    tcph.GetSeqNum(seqnum);
+    tcph.GetHeaderLen(tcph_size);
+    tcph.GetWinSize(window_size);
+    tcph.GetChecksum(checksum);
+    tcph.GetUrgentPtr(urgent);
+    tcph.GetAckNum(ack);
+
+	ConnectionList<TCPState>::iterator cs = clist.FindMatching(c);
+    //connection not found in list
+    if(cs==clist.end()){
+        //if this is a request for a new connection add connection to list, send ack
+        if(IS_SYN(flags)){
+            //need to select seqnum
+            //is this synax for enum?
+            //what is timertries
+            //TCPState tcp = new TCPState(startSeqNum, eState.SYN_RCVD, 1);
+            
+        }
+        //if it is not a request for a new connection and con doesnt exist then drop the packet
+        else{
+        }
+    }
+    //connection found in list
+    else{
+        //check TCPState
+        int currState = cs->state.GetState();
+        switch (currState){
+            case CLOSED:
+                //passive open, create TCB -> LISTEN
+                //active open, create TCB snd SYN -> SYN_SENT
+                break;
+            case LISTEN:
+                //Close, delete TCB ->CLOSED
+                //rcv SYN, send SYN, ACK -> SYN_RCVD
+                //SEND, snd SYN -> SYN_SENT
+                break;
+            case SYN_RCVD:
+                //CLOSE, snd FIN -> FIN_WAIT1
+                //rcv ACK of SYN -> ESTABLISHED
+                break;
+            case SYN_SENT:
+                //rcv SYN, snd ACK -> SYN_RCVD
+                //rcv SYN, ACK, snd ACK -> ESTABLISHED
+                break;
+            case SYN_SENT1:
+                //?
+                break;
+            case ESTABLISHED:
+                //CLOSE, snd FIN -> FIN_WAIT
+                break;
+            case SEND_DATA:
+                break;
+            case CLOSE_WAIT:
+                break;
+            case FIN_WAIT1:
+                break;
+            case CLOSING:
+                break;
+            case LAST_ACK:
+                break;
+            case FIN_WAIT2:
+                break;
+            case TIME_WAIT:
+                break;
+            default:
+                break;
+        }
+        
+        //if this is a request for a new connection for a connection that exists, send ack for last valid seqnum??
+        if(IS_SYN(flags)){
+        
+        }
+        //update last_acked field for TCPState in connection list and update connection state
+        if(IS_ACK(flags)){
+        
+        }
+        //update TCP state, send ack
+        if(IS_FIN(flags)){
+            
+        }
+    }
+}
